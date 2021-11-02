dc998b76a3b42011ba0be3c891f56ff6ed4098d9
TheNetMasterAlex <thenetmasteralex@gmail.com>
2021-11-02 17:37:39 -0600

    Step 9: Component Configuration

--- walkthrough1/webapp/Component.js
+++ walkthrough1/webapp/Component.js
@@ -1,30 +1,36 @@
 sap.ui.define([
 	"sap/ui/core/UIComponent",
-	"sap/ui/Device",
-	"walkthrough1/model/models"
-], function (UIComponent, Device, models) {
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/model/resource/ResourceModel"
+ ], function (UIComponent, JSONModel, ResourceModel) {
 	"use strict";
-
 	return UIComponent.extend("walkthrough1.Component", {
-
-		metadata: {
-			manifest: "json"
-		},
-
-		/**
-		 * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
-		 * @public
-		 * @override
-		 */
-		init: function () {
-			// call the base component's init function
-			UIComponent.prototype.init.apply(this, arguments);
-
-			// enable routing
-			this.getRouter().initialize();
-
-			// set the device model
-			this.setModel(models.createDeviceModel(), "device");
-		}
+	   metadata : {
+		  "interfaces": ["sap.ui.core.IAsyncContentCreation"],
+		  "rootView": {
+			 "viewName": "walkthrough1.view.App",
+			 "type": "XML",
+			 /*"async": true, // implicitly set via the sap.ui.core.IAsyncContentCreation interface*/
+			 "id": "app"
+		  }
+	   },
+	   init : function () {
+		  // call the init function of the parent
+		  UIComponent.prototype.init.apply(this, arguments);
+		  // set data model
+		  var oData = {
+			 recipient : {
+				name : "El Mundo"
+			 }
+		  };
+		  var oModel = new JSONModel(oData);
+		  this.setModel(oModel);
+ 
+		  // set i18n model
+		  var i18nModel = new ResourceModel({
+			 bundleName: "walkthrough1.i18n.i18n"
+		  });
+		  this.setModel(i18nModel, "i18n");
+	   }
 	});
-});
+ });
--- walkthrough1/webapp/index.js
+++ walkthrough1/webapp/index.js
@@ -1,12 +1,13 @@
 sap.ui.define([
-	"sap/ui/core/mvc/XMLView"
-], function (XMLView) {
+	"sap/ui/core/ComponentContainer"
+], function (ComponentContainer) {
 	"use strict";
 
-	XMLView.create({
-		viewName: "walkthrough1.view.App"
-	}).then(function (oView) {
-		oView.placeAt("content");
-	});
-
+	new ComponentContainer({
+		name: "walkthrough1",
+		settings : {
+			id : "walkthrough"
+		},
+		async: true
+	}).placeAt("content");
 });
