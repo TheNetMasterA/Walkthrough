95530fa4167b81e1629d06741fcabcd33357dd5a
TheNetMasterAlex <thenetmasteralex@gmail.com>
2021-11-04 08:25:54 -0600

    Step 28: Integration Test with OPA

--- walkthrough1/webapp/test/integration/NavigationJourney.js
+++ walkthrough1/webapp/test/integration/NavigationJourney.js
@@ -1,21 +1,32 @@
-/*global QUnit*/
+/*global QUnit, opaTest*/
 
 sap.ui.define([
+	"walkthrough1/localService/mockserver",
 	"sap/ui/test/opaQunit",
-	"./pages/Principal"
-], function (opaTest) {
+	"./pages/App"
+], function (mockserver) {
 	"use strict";
 
-	QUnit.module("Navigation Journey");
+	QUnit.module("Navigation");
 
-	opaTest("Should see the initial page of the app", function (Given, When, Then) {
+	opaTest("A verdad que mola? Dialogo Hola", function (Given, When, Then) {
+		// initialize the mock server
+		mockserver.init();
+
 		// Arrangements
-		Given.iStartMyApp();
+		Given.iStartMyUIComponent({
+			componentConfig: {
+				name: "walkthrough1"
+			}
+		});
+
+		//Actions
+		When.onTheAppPage.iPressTheSayHelloWithDialogButton();
 
 		// Assertions
-		Then.onTheAppPage.iShouldSeeTheApp();
+		Then.onTheAppPage.iShouldSeeTheHelloDialog();
 
-		//Cleanup
+		// Cleanup
 		Then.iTeardownMyApp();
 	});
 });
--- walkthrough1/webapp/test/integration/opaTests.qunit.js
+++ walkthrough1/webapp/test/integration/opaTests.qunit.js
@@ -1,10 +1,12 @@
 /* global QUnit */
+
 QUnit.config.autostart = false;
 
 sap.ui.getCore().attachInit(function () {
 	"use strict";
 
 	sap.ui.require([
+		"walkthrough1/test/integration/NavigationJourney"
 	], function () {
 		QUnit.start();
 	});
